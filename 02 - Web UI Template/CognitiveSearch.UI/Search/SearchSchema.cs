f36fa1581178a093e3b84deb136feaf465a797f1
Derek Legenzoff <delegenz@microsoft.com>
2019-08-29 14:40:07 -0500

    adding support for other collection types

--- 02 - Web UI Template/CognitiveSearch.UI/Search/SearchSchema.cs
+++ 02 - Web UI Template/CognitiveSearch.UI/Search/SearchSchema.cs
@@ -48,10 +48,17 @@ namespace CognitiveSearch.UI
             else if (field.Type == DataType.Int64) type = typeof(Int64);
             else if (field.Type == DataType.String) type = typeof(string);
             else if (field.Type == DataType.GeographyPoint) type = typeof(Microsoft.Spatial.GeographyPoint);
+   
             // Azure Search DataType objects don't follow value comparisons, so use overloaded string conversion operator to be a consistent representation
             else if ((string)field.Type == (string)DataType.Collection(DataType.String)) type = typeof(string[]);
             else if (field.Type == DataType.Complex) type = typeof(string);
             else if (field.Type == DataType.Collection(DataType.Complex)) type = typeof(string[]);
+            else if ((string)field.Type == (string)DataType.Collection(DataType.DateTimeOffset)) type = typeof(DateTime[]);
+            else if ((string)field.Type == (string)DataType.Collection(DataType.GeographyPoint)) type = typeof(Microsoft.Spatial.GeographyPoint[]);
+            else if ((string)field.Type == (string)DataType.Collection(DataType.Double)) type = typeof(double[]);
+            else if ((string)field.Type == (string)DataType.Collection(DataType.Boolean)) type = typeof(Boolean[]);
+            else if ((string)field.Type == (string)DataType.Collection(DataType.Int64)) type = typeof(Int32[]);
+            else if ((string)field.Type == (string)DataType.Collection(DataType.Int64)) type = typeof(Int64[]);
             else
             {
                 throw new ArgumentException($"Cannot map {field.Type} to a C# type");
